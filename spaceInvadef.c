52150da9967a29463a83885b798564217e32fa48
Geordie Jones <geordiejonesnufc@icloud.com>
2025-04-22 09:55:58 -0400

    inital creation of program

---
+++ spaceInvadef.c
@@ -0,0 +1,50 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+int main(){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Space Invader", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_RaiseWindow(window);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer){
+        printf("SDL failied to create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+
+    SDL_Event event;
+    bool quit = false;
+    while(!quit){
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                quit = true;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0,0,0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0,0,255, 255);
+        SDL_Rect box = {200,200,200, 200};
+        SDL_RenderFillRect(renderer, &box);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(50);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
+    return 0;
+}
